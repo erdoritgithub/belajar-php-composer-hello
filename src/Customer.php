6fd6dea73683d161fec13616e711168ee3577ba2
edoro <erdoronal28@gmail.com>
2023-05-25 14:37:15 +0700

    upgrade customer.php

--- src/Customer.php
+++ src/Customer.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Erdoro\Belajar;
+namespace Erdoro\BelajarPhpComposerHello;
 
 class Customer{
     public function __construct(private string $name){
 
     }
 
-    public function sayHello(string $name): string{
+    public function sayHello(string $name="Guest"): string{
         return "Hello $name, My Name is $this->name";
     }
 }
